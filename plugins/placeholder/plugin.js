619273c8727370a48e847131b5b9a63363bbc36f
Ilshat G <inlinegi@gmail.com>
2015-08-26 13:55:42 +0500

    placeholder

--- plugins/placeholder/plugin.js
+++ plugins/placeholder/plugin.js
@@ -1,43 +1,65 @@
-tinymce.PluginManager.add('placeholder', function(editor) {
-    editor.on('init', function() {
-        var label = new Label;
-        
-        onBlur();
+tinymce.PluginManager.add('placeholder', function (editor) {
+	editor.on('init', function () {
+		var label = new Label;
 
-        tinymce.DOM.bind(label.el, 'click', onFocus);
-        editor.on('focus', onFocus);
-        editor.on('blur', onBlur);
+		onBlur();
 
-        function onFocus(){
-            label.hide();
-            tinyMCE.execCommand('mceFocus', false, editor);
-        }
+		tinymce.DOM.bind(label.el, 'click', onFocus);
+		editor.on('focus', onFocus);
+		editor.on('blur', onBlur);
+		editor.on('change', onChange);
 
-        function onBlur(){
-            if(editor.getContent() == '') {
-                label.show();
-            }else{
-                label.hide();
-            }
-        }
-    });
+		function onChange() {
+			if (editor.getContent() == '') {
+				label.show();
+			} else {
+				label.hide();
+			}
+		}
 
-    var Label = function(){
-        // Create label el
-        this.text = editor.getElement().getAttribute("placeholder");
-        this.contentAreaContainer = editor.getContentAreaContainer();
+		function onFocus() {
+			label.hide();
+			tinyMCE.execCommand('mceFocus', false, editor);
+		}
 
-        tinymce.DOM.setStyle(this.contentAreaContainer, 'position', 'relative');
+		function onBlur() {
+			if (editor.getContent() == '') {
+				label.show();
+			} else {
+				label.hide();
+			}
+		}
+	});
 
-        attrs = {style: {position: 'absolute', top:'5px', left:0, color: '#888', padding: '1%', width:'98%', overflow: 'hidden'} };
-        this.el = tinymce.DOM.add( this.contentAreaContainer, "label", attrs, this.text );
-    }
+	var Label = function () {
+		// Create label el
+		this.text = editor.getElement().getAttribute("placeholder");
+		this.contentAreaContainer = editor.getContentAreaContainer();
 
-    Label.prototype.hide = function(){
-        tinymce.DOM.setStyle( this.el, 'display', 'none' );
-    }
+		tinymce.DOM.setStyle(this.contentAreaContainer, 'position', 'relative');
 
-    Label.prototype.show = function(){
-        tinymce.DOM.setStyle( this.el, 'display', '' );   
-    }
+		attrs = {
+			style: {
+				position: 'absolute',
+				top: '5px',
+				left: '11px',
+				color: '#9A9A9A',
+				padding: '1%',
+				width: '98%',
+				overflow: 'hidden',
+				'font-family': "'Open Sans', Arial",
+				'font-size': '14px',
+				'font-weight': 300
+			}
+		};
+		this.el = tinymce.DOM.add(this.contentAreaContainer, "label", attrs, this.text);
+	}
+
+	Label.prototype.hide = function () {
+		tinymce.DOM.setStyle(this.el, 'display', 'none');
+	}
+
+	Label.prototype.show = function () {
+		tinymce.DOM.setStyle(this.el, 'display', '');
+	}
 });
